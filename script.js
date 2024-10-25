57a2774286fa67df13a4d1ee5f6a47dd50ec094f
Matthew Vuong <matthewevuong@gmail.com>
2024-10-25 11:40:44 -0400

    added js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const element = document.getElementById("chf");
+element.addEventListener("click", heronFormula);
+
+function heronFormula() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
